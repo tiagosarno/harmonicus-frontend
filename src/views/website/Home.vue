acd61ac44a06c763867d70c892a02c1d89e72355
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-05-26 11:27:22 -0300

    Estrutura inicial da plataforma

---
+++ src/views/website/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <b-container fluid class="mt-3">
+
+    <b-card-group deck>
+      <b-card no-body>
+        <b-card-body>
+          This is a article container.
+        </b-card-body>        
+      </b-card>
+      <b-card no-body>
+        <b-card-body>
+          This is a wellcome container.
+        </b-card-body>        
+      </b-card>      
+    </b-card-group>
+
+    <b-card-group deck class="mt-3">
+      <b-card no-body>
+        <b-card-body>
+          This is a video container.
+        </b-card-body>        
+      </b-card>
+      <b-card no-body>
+        <b-card-body>
+          This is a who is container.
+        </b-card-body>
+      </b-card>
+    </b-card-group>
+
+  </b-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.internal {
+  display: flex;
+  height: 650px;
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
