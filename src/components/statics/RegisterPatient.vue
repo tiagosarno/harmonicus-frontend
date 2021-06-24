dee052d0a55e689c9852015781232b74a45cf1ff
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-06-24 08:46:48 -0300

    Correções formulário paciente

--- src/components/statics/RegisterPatient.vue
+++ src/components/statics/RegisterPatient.vue
@@ -8,13 +8,17 @@
       </li>
       <li>
         Não serão cobradas taxas nem mensalidades por este cadastro, com ele 
-        você terá acesso ao seu painel de consultas e gerenciar os seus 
-        agendamentos.
+        você terá acesso ao seu painel de consultas na plataforma.
       </li> 
       <li>
-        Todos os pagamentos de suas consultas ocorrem em <strong>ambiente 100% 
-        seguro</strong>, cumprindo as melhores políticas de privacidade e 
-        segurança.
+        Todos os seus pagamentos de consultas ocorrerão em um ambiente 
+        seguro, cumprindo todas as normas da lei 
+        <b-link 
+          href="http://www.planalto.gov.br/ccivil_03/_ato2015-2018/2018/lei/l13709.htm"
+          target="_blank"
+          v-b-tooltip.hover
+          title="Acessar lei geral de proteção de dados (LGPD)">
+          (LGPD)</b-link>.
       </li>
     </ul>
   </div>
