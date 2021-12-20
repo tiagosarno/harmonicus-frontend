e2a1f48ba3c5bb67d4a32b52d07e4b7b7a283a7c
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-12-20 17:11:07 -0300

    correções importantes na dashboard

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,12 +1,12 @@
 <template>
   <b-container class="mt-3" fluid>
-    <b-alert show>
-      <strong>Olá, seja bem vindo!</strong> Selecione o item desejado no menu acima.
+    <b-alert class="user-select-none" show>
+      <strong>Olá, seja bem vindo!</strong> Selecione a opção desejada no menu acima.
     </b-alert>
     <b-card 
-      class="text-center"
+      class="text-center user-select-none"
       footer="Copyright © 2021. Todos os direitos reservados">
-      <img src="@/assets/harmonicus.png" />
+      <img src="@/assets/harmonicus.png" width="300" />
     </b-card>
   </b-container>
 </template>
